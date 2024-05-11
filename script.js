9a8cba02d98e9e751b34622b74f84e834dbf099a
Aditya Gupta <ag1741923@gmail.com>
2024-05-11 16:09:24 +0530

    adding website files

---
+++ script.js
@@ -0,0 +1,10 @@
+function toggleMenu() {
+    var menu =
+    document.getElementById("flyoutMenu");
+    menu.classList.toggle("hidden");
+        }
+function closeMenu() {
+    const flyoutMenu =
+    document.getElementById('flyoutMenu');
+    flyoutMenu.classList.add('hidden');
+}
